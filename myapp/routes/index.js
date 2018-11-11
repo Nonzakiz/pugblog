f770f550b086b75f0378b300477311fc8f9fd736
Tawan Rungsang <s5601081616453@email.kmutnb.ac.th>
2018-11-11 13:03:16 +0700

    find data

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -4,19 +4,27 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) =>{
-  res.render('index', { title: 'Nonzakiz' });
+  Article.find((err, docs)=>{
+    res.render('index', { docs });
+   });
 });
 
 router.get('/add', (req, res, next) =>{
   res.render('add');
   });
 
-  router.post('/save',  (req, res)=> {
+router.post('/save',  (req, res)=> {
     const myData = new Article(req.body);
     myData.save()
     .then(item => {
-      res.render('save',{title:'เพิ่มบทความเรียบร้อย!'});
-    })
- })
+    res.render('save',{title:'เพิ่มบทความเรียบร้อย!'});
+  })
+})
+router.get('/show/:id',(req, res)=>{
+  const id = req.params.id
 
+      Article.findOne({_id: id},(err, docs)=>{
+     res.render('layout', { idtitle:docs._id,title:docs.title,detail:docs.detail });
+      });
+  });
 module.exports = router;
